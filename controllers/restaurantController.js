e8b01294a8512d8a04f5388d28b0e62d9acafb14
Roberto Ffrench-Davis <roberto_ffrench@hotmail.com>
2020-09-16 19:37:58 -0300

    Restaurant reservations done

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,31 +2,98 @@
 
 // Toda API REST debe enviar siempre el código HTTP de respuesta dependiendo el resultado de esta. Por ej: 201 se creó algo, 403 Forbidden, 200 OK, etc
 
-const checkUserRole = require('../util/checkUserRole');
+const axios = require('axios');
+const checkRoles = require('../util/checkRoles'); // archivo importado que tiene funciones para chequear cada rol
 
 
-exports.getReservations = (req, res, next) => {
-    checkUserRole(req.roleId);
+
+// RESERVAS
+// Vistas Reservas
+exports.getReservationsMenu = (req, res, next) => {
+    checkRoles.checkIfCustomer(req.roleId);
     res.status(200).send('Cliente autorizado con ID: ' + req.userId);
 }
 
 exports.getNewReservation = (req, res, next) => {
-    checkUserRole(req.roleId);
+    checkRoles.checkIfCustomer(req.roleId);
     res.status(200).send('Cliente autorizado con ID: ' + req.userId);
 }
+
+// Endpoints reservas
+exports.getReservations = (req, res, next) => {
+    axios.get(`${process.env.RESTAURANT}/reservations`)
+        .then(response => {
+            console.log(response.data);
+
+            res.status(201).json(response.data);
+        })
+        .catch(err => {
+            next(err);
+        })
+}
+
+
+exports.getReservation = (req, res, next) => {
+    const userId = req.params.userId;
+
+    axios.get(`${process.env.RESTAURANT}/reservations/${userId}`)
+        .then(response => {
+            console.log(response.data);
+
+            res.status(201).json(response.data);
+        })
+        .catch(err => {
+            next(err);
+        })
+}
+
+exports.postReservation = (req, res, next) => {
+    const [reservationDate, reservationTime, party, userId] = [req.body.reservationDate, req.body.reservationTime, req.body.party, req.body.userId];
+
+
+    axios.post(`${process.env.RESTAURANT}/reservations`,
+        {
+            reservationDate: reservationDate,
+            reservationTime: reservationTime,
+            party: party,
+            userId: userId
+        })
+        .then(response => {
+            res.status(201).json(response.data);
+        })
+        .catch(err => {
+            next(err);
+        })
+
+}
+
+exports.deleteReservation = (req, res, next) => {
+    const userId = req.params.userId;
+
+    axios.delete(`${process.env.RESTAURANT}/reservations/${userId}`)
+        .then(response => {
+            res.status(201).json(response.data);
+        })
+        .catch(err => {
+            next(err);
+        })
+
+}
+
+
 
 
 
 
 // Orders
 
-exports.getOrders = (req, res, next) => {
-    checkUserRole(req.roleId);
+exports.getOrdersMenu = (req, res, next) => {
+    checkRoles.checkIfCustomer(req.roleId);
     res.status(200).send('Cliente autorizado con ID: ' + req.userId);
 }
 
 exports.getNewOrder = (req, res, next) => {
-    checkUserRole(req.roleId);
+    checkRoles.checkIfCustomer(req.roleId);
     res.status(200).send('Cliente autorizado con ID: ' + req.userId);
 }
 
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -6,10 +6,21 @@ const restaurantController = require('../controllers/restaurantController');
 const isAuth = require('../middleware/isAuth');
 
 // isAuth es un middleware que se ejecuta antes para verificar el token y si está autenticado el usuario o si tiene Autorización para acceder a ese recurso
-router.get('/reservations', isAuth, restaurantController.getReservations)
-router.get('/reservations/new', isAuth, restaurantController.getNewReservation);
 
-router.get('/orders', isAuth, restaurantController.getOrders);
+// Rutas vistas reservas para ver si se le carga la pag al usuario o no
+router.get('/reservations/menu', isAuth, restaurantController.getReservationsMenu)
+router.get('/reservations/new', isAuth, restaurantController.getNewReservation);
+// CRD Reservas
+router.get('/reservations', restaurantController.getReservations);
+router.post('/reservations', restaurantController.postReservation);
+router.get('/reservations/:userId', restaurantController.getReservation);
+router.delete('/reservations/:userId', restaurantController.deleteReservation);
+
+
+// Rutas vistas ordenes
+router.get('/orders/menu', isAuth, restaurantController.getOrdersMenu);
 router.get('/orders/new', isAuth, restaurantController.getNewOrder)
+
+
 
 module.exports = router;
