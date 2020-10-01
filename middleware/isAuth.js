52507e508a210baa53274e7aa67fcca6bb578a8e
Roberto Ffrench-Davis <roberto_ffrench@hotmail.com>
2020-09-30 23:55:30 -0300

    token 401 bug fixed

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -9,20 +9,24 @@ module.exports = (req, res, next) => {
         error.statusCode = 401;
         throw error;
     }
+
     const token = authHeader.split(' ')[1]; // Bearer va primero y luego el token se obtiene
     let decodedToken;
 
-    try {
-        decodedToken = jwt.verify(token, 'llavetoken'); // El secret debería ir en un ENV
+    try { // if there is a token verify it. If it is null it will come as a string so it should be set to null
+        decodedToken = token !== 'null' ? jwt.verify(token, 'llavetoken') : null; // El secret debería ir en un ENV
+
     } catch (err) {
         err.statusCode = 500;
         throw err;
     }
-    if (!decodedToken) {
+
+    if (!decodedToken) { // if token is null then the user is not authenticated
         const error = new Error('Not authenticated.');
         error.statusCode = 401;
         throw error;
     }
+
     req.userId = decodedToken.userId;
     req.roleId = decodedToken.roleId;
     // agregar role ID
