006d8889f062f9ab97657062afdb2f23c710ac73
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2020-11-18 18:15:09 -0300

    Rutas para generar reportes

--- routes/financeRoutes.js
+++ routes/financeRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const financeController = require('../controllers/financeController');
 const isAuth = require('../middleware/isAuth');
 
-router.get('/incomes', isAuth, financeController.getIncomesView);
+router.get('/income', isAuth, financeController.getIncomeView);
+router.get('/income', financeController.getIncomeDates);
+router.get('/income/:month/:year', financeController.getDailyIncome);
 module.exports = router;
